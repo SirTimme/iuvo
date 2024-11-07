b6a8174ad1a2ae9ebe97bec1cc58253637c5f527
SirTimme <tim@sirtimme.dev>
2024-11-07 12:34:11 +0100

    implement MultiResourceBundle in iuvo

--- src/main/java/dev/sirtimme/iuvo/internal/localization/LocalizationManager.java
+++ src/main/java/dev/sirtimme/iuvo/internal/localization/LocalizationManager.java
@@ -1,5 +1,6 @@
 package dev.sirtimme.iuvo.internal.localization;
 
+import dev.sirtimme.iuvo.api.localization.MultiResourceBundle;
 import net.dv8tion.jda.api.interactions.DiscordLocale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,16 +8,18 @@ import org.slf4j.LoggerFactory;
 import java.text.MessageFormat;
 import java.util.*;
 
+import static dev.sirtimme.iuvo.api.listener.interaction.InteractionListener.USER_LOCALE;
+
 public class LocalizationManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(LocalizationManager.class);
-    private static final Map<DiscordLocale, ResourceBundle> bundles = new HashMap<>();
+    private static final HashMap<DiscordLocale, MultiResourceBundle> bundles = new HashMap<>();
 
     static {
         addBundles(DiscordLocale.ENGLISH_US, DiscordLocale.GERMAN);
     }
 
-    public static String getResponse(final String key, final DiscordLocale locale, final Object... values) {
-        final var bundle = getBundle(locale);
+    public static String getResponse(final String key, final Object... values) {
+        final var bundle = bundles.get(USER_LOCALE.get());
         final var template = new MessageFormat(bundle.getString(key));
 
         return template.format(values);
@@ -26,23 +29,14 @@ public class LocalizationManager {
         for (final var discordLocale : discordLocales) {
             final var locale = discordLocale.toLocale();
             try {
-                final var resourceBundle = ResourceBundle.getBundle("localization/responses", locale);
-                bundles.put(discordLocale, resourceBundle);
+                final var responseBundle = ResourceBundle.getBundle("localization/responses", locale);
+                final var combinedBundle = new MultiResourceBundle(responseBundle);
 
-                LOGGER.info("Loaded bundle for locale '{}'", locale);
+                bundles.put(discordLocale, combinedBundle);
+                LOGGER.info("Loaded bundles for locale '{}'", locale);
             } catch (final MissingResourceException error) {
-                LOGGER.error("Loading the bundle for locale '{}' failed: {}", locale, error.getMessage());
+                LOGGER.warn("Loading of bundles for locale '{}' failed: {}", locale, error.getMessage());
             }
         }
-    }
-
-    private static ResourceBundle getBundle(final DiscordLocale locale) {
-        final var bundle = bundles.get(locale);
-
-        if (bundle == null) {
-            return bundles.get(DiscordLocale.ENGLISH_US);
-        }
-
-        return bundle;
     }
 }
