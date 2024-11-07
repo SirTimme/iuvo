be50d252fd2849a0c87e4a0e194bc1442d87846d
SirTimme <tim@sirtimme.dev>
2024-11-07 11:44:37 +0100

    introduce scoped value for user locale

--- src/main/java/dev/sirtimme/iuvo/api/listener/interaction/InteractionListener.java
+++ src/main/java/dev/sirtimme/iuvo/api/listener/interaction/InteractionListener.java
@@ -7,7 +7,10 @@ import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Locale;
+
 public class InteractionListener<T extends GenericInteractionCreateEvent> extends ListenerBase<T> {
+    public final static ScopedValue<Locale> USER_LOCALE = ScopedValue.newInstance();
     private static final Logger LOGGER = LoggerFactory.getLogger(InteractionListener.class);
     private final EntityManagerFactory entityManagerFactory;
     private final IInteractionCommandFactory<T> commandFactory;
@@ -23,19 +26,23 @@ public class InteractionListener<T extends GenericInteractionCreateEvent> extend
         final var context = entityManagerFactory.createEntityManager();
         final var command = commandFactory.createCommand(event, context);
 
-        if (command.hasInvalidPreconditions(event)) {
-            return;
-        }
+        ScopedValue.where(USER_LOCALE, event.getUserLocale().toLocale()).run(
+            () -> {
+                if (command.hasInvalidPreconditions(event)) {
+                    return;
+                }
 
-        try {
-            context.getTransaction().begin();
-            command.execute(event);
-            context.getTransaction().commit();
-        } catch (Exception error) {
-            LOGGER.error("Execution of command {} failed: {}", command, error.getMessage());
-            context.getTransaction().rollback();
-        } finally {
-            context.close();
-        }
+                try {
+                    context.getTransaction().begin();
+                    command.execute(event);
+                    context.getTransaction().commit();
+                } catch (Exception error) {
+                    LOGGER.error("Execution of command {} failed: {}", command, error.getMessage());
+                    context.getTransaction().rollback();
+                } finally {
+                    context.close();
+                }
+            }
+        );
     }
 }
