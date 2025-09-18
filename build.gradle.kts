97b310b7d375f24bf46e500814fc3c1e05b4c50e
Tim Piechowicz <tim@sirtimme.dev>
2025-09-18 23:29:27 +0200

    fix(gradle): move publishing logic back to root gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `java-library`
+    `maven-publish`
     id("dev.sirtimme.gradle.java-conventions")
-    id("dev.sirtimme.gradle.publishing-conventions")
 }
 
 group = "dev.sirtimme"
@@ -17,4 +17,68 @@ dependencies {
     }
     api("org.hibernate:hibernate-core:7.0.3.Final")
     api("io.github.classgraph:classgraph:4.8.180")
+}
+
+fun MavenPom.populate() {
+    name = "iuvo"
+    description = "Small collection of interfaces to make developing bots with JDA quicker"
+    url = "https://github.com/SirTimme/iuvo"
+    licenses {
+        license {
+            name = "The MIT License (MIT)"
+            url = "https://mit-license.org/"
+        }
+    }
+    developers {
+        developer {
+            id = "SirTimme"
+            name = "Tim Piechowicz"
+            email = "tim@sirtimme.dev"
+        }
+    }
+    scm {
+        url = "https://github.com/SirTimme/iuvo"
+        connection = "scm:git:git://github.com/SirTimme/iuvo"
+        developerConnection = "scm:git:ssh:git@github.com:SirTimme/iuvo"
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "release"
+            url = uri("https://patient-turkey-beloved.ngrok-free.app/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+        maven {
+            name = "snapshot"
+            url = uri("https://patient-turkey-beloved.ngrok-free.app/snapshots")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("release") {
+            groupId = project.group as String
+            artifactId = project.name
+            version = project.version as String
+
+            from(components["java"])
+            pom.populate()
+        }
+        create<MavenPublication>("snapshot") {
+            groupId = project.group as String
+            artifactId = project.name
+            version = "${project.version}-SNAPSHOT"
+
+            from(components["java"])
+            pom.populate()
+        }
+    }
 }
