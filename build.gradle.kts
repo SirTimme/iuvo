69819b1621a4ae311a8a5bb788162a75926c1fcd
Tim Piechowicz <tim@sirtimme.dev>
2025-10-25 11:19:40 +0200

    feat: improve publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,66 +19,49 @@ dependencies {
     api("io.github.classgraph:classgraph:4.8.180")
 }
 
-fun MavenPom.populate() {
-    name = "iuvo"
-    description = "Small collection of interfaces to make developing bots with JDA quicker"
-    url = "https://github.com/SirTimme/iuvo"
-    licenses {
-        license {
-            name = "The MIT License (MIT)"
-            url = "https://mit-license.org/"
-        }
-    }
-    developers {
-        developer {
-            id = "SirTimme"
-            name = "Tim Piechowicz"
-            email = "tim@sirtimme.dev"
-        }
-    }
-    scm {
-        url = "https://github.com/SirTimme/iuvo"
-        connection = "scm:git:git://github.com/SirTimme/iuvo"
-        developerConnection = "scm:git:ssh:git@github.com:SirTimme/iuvo"
-    }
-}
-
 publishing {
-    repositories {
-        maven {
-            name = "release"
-            url = uri("https://artifactory.sirtimme.dev/releases")
-            credentials(PasswordCredentials::class)
-            authentication {
-                create<BasicAuthentication>("basic")
-            }
-        }
-        maven {
-            name = "snapshot"
-            url = uri("https://artifactory.sirtimme.dev/snapshots")
-            credentials(PasswordCredentials::class)
-            authentication {
-                create<BasicAuthentication>("basic")
-            }
-        }
-    }
-
     publications {
-        create<MavenPublication>("release") {
-            groupId = project.group as String
+        register<MavenPublication>("release") {
+            from(components["java"])
+
             artifactId = project.name
+            groupId = project.group as String
             version = project.version as String
 
-            from(components["java"])
-            pom.populate()
+            pom {
+                name = "iuvo"
+                description = "Small collection of interfaces to make developing bots with JDA quicker"
+                url = "https://github.com/SirTimme/iuvo"
+                licenses {
+                    license {
+                        name = "The MIT License (MIT)"
+                        url = "https://mit-license.org/"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "SirTimme"
+                        name = "Tim Piechowicz"
+                        email = "tim@sirtimme.dev"
+                    }
+                }
+                scm {
+                    url = "https://github.com/SirTimme/iuvo"
+                    connection = "scm:git:git://github.com/SirTimme/iuvo"
+                    developerConnection = "scm:git:ssh:git@github.com:SirTimme/iuvo"
+                }
+            }
         }
-        create<MavenPublication>("snapshot") {
-            groupId = project.group as String
-            artifactId = project.name
-            version = "${project.version}-SNAPSHOT"
+    }
+    repositories {
+        maven {
+            val releaseUrl = uri("https://artifactory.sirtimme.dev/releases")
+            val snapshotUrl = uri("https://artifactory.sirtimme.dev/snapshots")
 
-            from(components["java"])
-            pom.populate()
+            name = "artifactory"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotUrl else releaseUrl)
+
+            credentials(PasswordCredentials::class)
         }
     }
 }
